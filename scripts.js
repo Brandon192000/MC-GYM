19f3be348a0794bb5dadf921b15eb6bd150ad45e
Brandon192000 <brandonblanco938@gmail.com>
2024-10-28 19:44:09 -0600

    Cambio navba size issue

--- scripts.js
+++ scripts.js
@@ -10,6 +10,29 @@ function moveSlide(direction) {
     // Mueve el contenedor de las imágenes para mostrar la imagen actual
     const offset = -currentSlide * 100; // Calcula el desplazamiento basado en el ancho del contenedor
     slides.style.transform = `translateX(${offset}%)`;
+
+
+    // Función para alternar la visualización de las listas
+function toggleList(viewMoreBtnId, titlesListId) {
+    const viewMoreBtn = document.getElementById(viewMoreBtnId);
+    const titlesList = document.getElementById(titlesListId);
+    let isExpanded = false;
+  
+        viewMoreBtn.addEventListener("click", function() {
+        if (isExpanded) {
+            titlesList.classList.remove("expanded");
+            viewMoreBtn.textContent = "Ver más";
+        } else {
+            titlesList.classList.add("expanded");
+            viewMoreBtn.textContent = "Ver menos";
+        }
+        isExpanded = !isExpanded;
+        });
+    }
+  
+    // Añadir la funcionalidad a cada entrenador
+    toggleList("viewMoreBtn1", "titlesList1");
+    toggleList("viewMoreBtn2", "titlesList2");
 }
 
 
