5f29fad35ef2afa7b81c920e7224128e57f0dd8f
Brandon192000 <brandonblanco938@gmail.com>
2024-11-06 03:32:40 -0600

    cambio carrucel a materialize

--- scripts.js
+++ scripts.js
@@ -1,29 +1,30 @@
-// Variables globales para el carrusel
-let currentSlide = 0;
 
-// Función para mover el carrusel en una dirección específica (avanzar o retroceder)
-function moveSlide(direction) {
-    const slides = document.querySelector('.carousel-slide'); // Selecciona el contenedor que contiene las imágenes
-    const totalSlides = slides.children.length; // Número total de imágenes en el carrusel
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.carousel');
+  var instances = M.Carousel.init(elems, {
+      fullWidth: true,
+      indicators: true
+  });
 
-    // Actualiza el índice de la diapositiva actual
-    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
-
-    // Mueve el contenedor de las imágenes para mostrar la imagen actual
-    const offset = -currentSlide * 100; // Calcula el desplazamiento basado en el ancho del contenedor
-    slides.style.transform = `translateX(${offset}%)`;
-}
-
-// Lógica del menú burger
-const burger = document.getElementById('burger');
-const navbar = document.querySelector('.navbar');
-
-// Evento para el botón burger
-burger.addEventListener('click', function() {
-    // Alterna la clase 'active' en el botón burger y la barra de navegación
-    burger.classList.toggle('active');
-    navbar.classList.toggle('active');
+  // Función para mover el carrusel
+  window.moveCarousel = function(direction) {
+      var instance = M.Carousel.getInstance(elems[0]);
+      if (direction === 'next') {
+          instance.next();
+      } else if (direction === 'prev') {
+          instance.prev();
+      }
+  };
 });
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+
 
  // Seleccionamos el botón y la lista
  const viewMoreBtn = document.getElementById("viewMoreBtn");
